c59de735fbd3d98890ebaf36af544cf14f3d44e0
Myles Starr <mylesstarr@gmail.com>
2021-05-05 19:57:27 -0400

    eightpm assignment

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 //constants 
 
 const newToyForm = document.querySelector("form.add-toy-form")
-const textUserTyped = document.querySelector("#whatUserTypes")
-const imageUserTyped = document.querySelector("#urlUserTypes")
+// const textUserTyped = document.querySelector("#whatUserTypes")
+// const imageUserTyped = document.querySelector("#urlUserTypes")
 const toyCollection = document.querySelector("#toy-collection")
-
+const addToyForm = document.querySelector("form.add-toy-form")
 //getRequests
 
 function fetchAll () {
   fetch('http://localhost:3000/toys')
   .then(res => res.json())
   .then(toysArray => renderToy(toysArray))
-}
+};
 
+// function addNewPokemon(newPokemon) {
+//   fetch('http://localhost:3000/toys',
+//    {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(newPokemon),
+// }) }
+  // .then((r) => r.json())
+  // .then((articleObj) => console.log(articleObj)); }// this is for confirmation so we can cosole log and add it to the page 
 
+  // // newToyForm.addEventListener("submit", addToy(e)){
+  
+  
 
 let addToy = false; // boolean set at false
 
@@ -35,26 +49,30 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-
-newToyForm.addEventListener("submit", (event) => 
-{ event.preventDefault()
+//helper functions  
+function addNewToy(e){
+  e.preventDefault()
   
-  console.log(event)
+  const newPokemon ={
+    "name": e.target.whatUserTypes.value,
+    "url": e.target.urlUserTypes.value
+ }
 
-})
-
-// function addToy() {
-  
 
 function renderToy(toysArray) {
     toysArray.forEach(toy => {
-      console.log(toy)
+      // console.log(toy)
+      // console.log(toy.name)
+  
      const cardDiv = document.createElement("div")
-     cardDiv.className= ("card")
-      cardDiv.textContent = toy["name"].value
-      console.log(cardDiv.textContent)
-      cardDiv.append(h2);
+     cardDiv.innerHTML = `<div class="card">
+     <h2>${toy.name}</h2>
+     <img src="${toy.image}" class="toy-avatar" />
+     <p>${toy.likes} Likes </p>
+     <button class="like-btn" id="[toy_id]">Like <3</button>
+   </div>` 
+      toyCollection.append(cardDiv)
     });
+
   }
-
-
+}
