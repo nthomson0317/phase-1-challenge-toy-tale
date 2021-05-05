6ae861df01504e552abd5f77dadd1ddc3f1e331a
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-05-05 17:34:01 -0400

    nt commit 2

--- src/index.js
+++ src/index.js
@@ -3,16 +3,26 @@
 const newToyForm = document.querySelector("form.add-toy-form")
 const textUserTyped = document.querySelector("#whatUserTypes")
 const imageUserTyped = document.querySelector("#urlUserTypes")
-const toyCollection =document.querySelector("#toy-collection")
+const toyCollection = document.querySelector("#toy-collection")
 
 //getRequests
+
+function fetchAll () {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toysArray => renderToy(toysArray))
+}
+
 
 
 let addToy = false; // boolean set at false
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchAll()
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -31,10 +41,22 @@ newToyForm.addEventListener("submit", (event) =>
   
   console.log(event)
 
-
-function addToy() {
-  
-newToyForm.appendChild("li")
-}
-
 })
+
+// function addToy() {
+  
+
+function renderToy(toysArray) {
+  console.log(toysArray)
+  debugger;
+    toysArray.forEach(toy => {
+      
+     const cardDiv = document.createElement("div")
+     cardDiv.className= ("card")
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      toyCollection.append(h2);
+    });
+  }
+
+
