763894d2aca34411b972f4c30f611f2f1f14838e
Myles Starr <mylesstarr@gmail.com>
2021-05-05 17:08:03 -0400

    Myles First Draft

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-let addToy = false;
+//constants 
+
+const newToyForm = document.querySelector("form.add-toy-form")
+const textUserTyped = document.querySelector("#whatUserTypes")
+const imageUserTyped = document.querySelector("#urlUserTypes")
+const toyCollection =document.querySelector("#toy-collection")
+
+//getRequests
+
+
+let addToy = false; // boolean set at false
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -15,4 +25,16 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-change
+
+newToyForm.addEventListener("submit", (event) => 
+{ event.preventDefault()
+  
+  console.log(event)
+
+
+function addToy() {
+  
+newToyForm.appendChild("li")
+}
+
+})
